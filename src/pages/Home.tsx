904a5fb48d127f4d5d7b3f7d8172bf870425d011
simonlee-test <simonlee190301@gmail.com>
2026-06-20 22:30:56 +0800

    add nav bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ThemeToggle from '../components/ThemeToggle'
 import StarBackground from '../components/StarBackground'
+import Navbar from '../components/Navbar'
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
     <StarBackground/>
 
     {/* Navbar */}
+    <Navbar/>
 
     {/* Main Content */}
 
