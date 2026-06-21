47fbaf8c0836e518beeb5c1dd8b1f4b3e86a98d2
simonlee-test <simonlee190301@gmail.com>
2026-06-21 23:42:24 +0800

    add hero section title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ThemeToggle from '../components/ThemeToggle'
 import StarBackground from '../components/StarBackground'
 import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
+
 
 const Home = () => {
   return (
@@ -16,7 +18,9 @@ const Home = () => {
     <Navbar/>
 
     {/* Main Content */}
-
+    <main>
+      <HeroSection/>
+    </main>
     {/* Footer */}
     </div>
   )
