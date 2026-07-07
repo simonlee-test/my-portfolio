d6c30cb1ca3279d69997bf51978493dd1971a98c
simonlee-test <simonlee190301@gmail.com>
2026-07-07 12:54:40 +0800

    add project section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection'
 import AboutSection from '../components/AboutSection'
 import SkillsSection from '../components/SkillsSection'
+import ProjectsSection from '../components/ProjectsSection'
 
 
 const Home = () => {
@@ -24,6 +25,7 @@ const Home = () => {
       <HeroSection/>
       <AboutSection/>
       <SkillsSection />
+      <ProjectsSection/>
     </main>
     {/* Footer */}
     </div>
