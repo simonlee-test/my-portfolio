e39e59d2a54b1d719a1585cd851bae2198ca3f51
simonlee-test <simonlee190301@gmail.com>
2026-08-20 18:00:32 +0800

    feat: add footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ import AboutSection from '../components/AboutSection'
 import SkillsSection from '../components/SkillsSection'
 import ProjectsSection from '../components/ProjectsSection'
 import ContactSection from '../components/ContactSection'
+import Footer from '@/components/Footer'
 
 
 const Home = () => {
@@ -30,6 +31,7 @@ const Home = () => {
       <ContactSection/>
     </main>
     {/* Footer */}
+    <Footer/>
     </div>
   )
 }
