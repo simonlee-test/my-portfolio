eaacfc7e3f29b7689bcc5299c259fcec01c1cbd7
simonlee-test <simonlee190301@gmail.com>
2026-08-20 22:58:09 +0800

    refactor: edit skill section

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -10,24 +10,26 @@ interface skill {
 
 const skills: skill[] = [
   // Frontend
-  { name: "HTML/CSS", level: 95, category: "frontend" },
-  { name: "JavaScript", level: 55, category: "frontend" },
-  { name: "React", level: 55, category: "frontend" },
-  { name: "TypeScript", level: 55, category: "frontend" },
+  { name: "HTML/CSS", level: 65, category: "frontend" },
+  { name: "JavaScript", level: 50, category: "frontend" },
+  { name: "React", level: 40, category: "frontend" },
+  { name: "TypeScript", level: 60, category: "frontend" },
   { name: "Tailwind CSS", level: 55, category: "frontend" },
   { name: "Next.js", level: 15, category: "frontend" },
 
   // Backend
-  { name: "Node.js", level: 95, category: "backend" },
-  { name: "Express", level: 65, category: "backend" },
-  { name: "MongoDB", level: 65, category: "backend" },
-  { name: "PostgreSQL", level: 95, category: "backend" },
-  { name: "GraphQL", level: 25, category: "backend" },
+  { name: "Django", level: 80, category: "backend" },
+  { name: "FastAPI", level: 65, category: "backend" },
+  { name: "MongoDB", level: 60, category: "backend" },
+  { name: "PostgreSQL", level: 70, category: "backend" },
+  { name: "MySQL", level: 70, category: "backend" },
 
   //Tools
-  { name: "Git/GitHub", level: 95, category: "tools" },
+  { name: "Git/GitHub", level: 85, category: "tools" },
   { name: "Docker", level: 75, category: "tools" },
-  { name: "Figma", level: 45, category: "tools" },
+  { name: "KiloCode", level: 70, category: "tools" },
+  { name: "Hermes", level: 50, category: "tools" },
+  { name: "Antigravity", level: 50, category: "tools" },
   { name: "VS Code", level: 85, category: "tools" },
 ];
 const categories: string[] = ["all", "frontend", "backend", "tools"];
